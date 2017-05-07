8387479f18bf0584358ece3de145c9243d4426b4
Masashi Motohashi <masashi6225@motohashisusumushi-no-macbook-pro.local>
2017-05-07 21:43:14 +0900

    started structuring

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -10,16 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    var timer: Timer!
+    var timer_sec: Float = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    func updateTimer (timer:Timer){
+        self.timer_sec += 2.0
+        self.timerLabel.text = String(format:"%.1f",timer_sec)
+    }
+    
+    @IBAction func startTimer(_ sender: Any){
+        //進むボタンを押すとTimer作成、始動
+        if self.timer == nil {
+        self.timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector:#selector(updateTimer), userInfo: nil, repeats: true)
+        } else {
+            self.timer.invalidate() //タイマーが再生されていた場合は停止させる
+            self.timer = nil //startTimer()の timer==nil　で判断するために timer=nilとしておく
+        }
+    }
+
+    @IBAction func resetTimer(_ sender:Any){
+        self.timer_sec = 0
+        self.timerLabel.text = String(format: "%.1f", self.timer_sec)
+        
+        if self.timer != nil {
+            self.timer.invalidate() //現在のタイマーを破棄
+            self.timer = nil   //startTimer()の timer==nil　で判断するために timer=nilとしておく
+        }
+    }
+    
 
 }
 
