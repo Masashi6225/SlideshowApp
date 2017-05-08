a12870430c141ce85b252f04b298f143e176ab36
Masashi Motohashi <masashi6225@motohashisusumushi-no-MacBook-Pro.local>
2017-05-08 22:26:46 +0900

    minor changes

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -10,17 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let imageArray = ["0.1png","0.2png","0.3png"]
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imageArray = ["paint 1.png","paint 2.png","paint 3.png"]
 
     var count = 0
     func nextImage() {
-        imageView.image = UIImage(named: photos[count])
+        imageView.image = UIImage(named: imageArray[count])
         count = count + 1
         if (count>2) {
             count = 0
         }
-    
-    
+        
     @IBOutlet weak var timerLabel: UILabel!
     
     var timer: Timer!
@@ -29,8 +30,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-    
+        }
+        
+        func didClickImageView(recognizer: UIGestureRecognizer) {
+            if let imageView = recognizer.view as? UIImageView {
+                let image = imageView.image
+                let nextViewController = /** 次のUIViewControllerのインスタンス */
+                    nextViewController.image = iamge
+                /** netViewController への遷移 */
+        
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -49,18 +57,13 @@ class ViewController: UIViewController {
             self.timer.invalidate() //タイマーが再生されていた場合は停止させる
             self.timer = nil //startTimer()の timer==nil　で判断するために timer=nilとしておく
         }
-    }
-
-    @IBAction func resetTimer(_ sender:Any){
-        self.timer_sec = 0
-        self.timerLabel.text = String(format: "%.1f", self.timer_sec)
-        
-        if self.timer != nil {
-            self.timer.invalidate() //現在のタイマーを破棄
-            self.timer = nil   //startTimer()の timer==nil　で判断するために timer=nilとしておく
-        }
-    }
     
+     //自動送りの間は進むと戻るボタンはタップ不可に
+        // button.isEnabled = true
+        
+        
+    }
+            
 
 }
 
